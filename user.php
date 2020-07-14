6b8c3c85bb4e7dd0cdc7f5ce81754aa97964f1f5
Yusuf Rilwan <akinyusuf5@gmail.com>
2020-07-14 11:56:27 +0100

    mysql hosted

--- user.php
+++ user.php
@@ -2,12 +2,12 @@
  $mail = $_POST['email'];
  
  // Database connection
- $conn = new mysqli('localhost','root','','test');
+ $conn = new mysqli('d9c88q3e09w6fdb2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','ptamhr1f32u7u8d6','gfsqgjg3qa5uau1n','w7g9d8ukscppdutc');
  if($conn->connect_error){
      echo "$conn->connect_error";
      die("Connection Failed : ". $conn->connect_error);
  } else {
-     $stmt = $conn->prepare("insert into fan(name) values(?)");
+     $stmt = $conn->prepare("insert into subscribers(name) values(?)");
      $stmt->bind_param("s", $mail);
      $execval = $stmt->execute();
      echo $execval;
